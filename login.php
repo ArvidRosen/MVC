026f1658e086a39f7fecc9537ec95ece66722efd
Kalle842 <kalle.freigie@elev.ga.ntig.se>
2023-09-19 09:26:34 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,15 +1,15 @@
 <?php
-session_name("login");
-session_start();
-require("users.sql");
-$msg = "";
+session_name("login");/* Session namn */
+session_start();/* Starta sessionen */
+require("sql.php");/* Hämtar koden som finns i sql.php */
+$msg = "";/* Variabel som är tom som används senare i if statment */
 if(isset($_POST["username"])){
-    $salt = '$6$rounds=5000$kebab$';
+    $salt = '$6$rounds=5000$kebab$';/* Saltet för kryptering */
     $data = sql("SELECT * FROM users WHERE user = :user AND pass = :pass;",[
-        ":user" => $_POST["username"],  
-        ":pass" => substr(crypt($_POST["password"], $salt), strlen($salt))
+        ":user" => $_POST["username"],/* hämtar upp informationen user */
+        ":pass" => substr(crypt($_POST["password"], $salt), strlen($salt))/* använder saltet för att kryptera lösenordet och sedan hämtar informationen från pass*/
     ]);
-    if(isset($data[0])){
+    if(isset($data[0])){/* If statment för att som skickar ett meddelande som beror på ifall man ger rätt lösenord och användarnamn*/
         $msg = "Loggades in";
         $_SESSION["user"] = $data[0]["user"]; 
     }else{
