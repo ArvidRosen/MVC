1ce09ec86275858df69ec70fb12beb48cea270d0
Kalle842 <kalle.freigie@elev.ga.ntig.se>
2023-09-19 09:12:06 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,23 @@
 <?php
-
+session_name("login");
+session_start();
+require("users.sql");
+$msg = "";
+if(isset($_POST["username"])){
+    $salt = '$6$rounds=5000$kebab$';
+    $data = sql("SELECT * FROM users WHERE user = :user AND pass = :pass;",[
+        ":user" => $_POST["username"],  
+        ":pass" => substr(crypt($_POST["password"], $salt), strlen($salt))
+    ]);
+    if(isset($data[0])){
+        $msg = "Loggades in";
+        $_SESSION["user"] = $data[0]["user"]; 
+    }else{
+        $msg = "Kunde inte logga in";
+    }   
+}
+    /* echo substr(crypt($_POST["password"], $salt), strlen($salt)); */
+   /*  echo "<pre>";
+    print_r($data);
+    echo "</pre>";  */
 ?>
