d7cc55fd42b38377a4650a8ebf15cfb1d7480cee
Ralukaaa222 <114411632+Ralukaaa222@users.noreply.github.com>
2023-10-03 11:10:22 +0200

    qwert

--- view.php
+++ view.php
@@ -1,38 +1,38 @@
 <?php
 
-class View { 
+class View { /* create a class by the name View*/
 
-    public static function form($acc) { 
-        if($acc == "register") {
+    public static function form($acc) { /*defines a static method named form that atkes one parameter $acc */
+        if($acc == "register") { /*checing if $acc is equal to register */
         ?>
-            <form action="index.php" method="POST">
-                <input type="text" name="user" placeholder="username">
-                <input type="password" name="pass" placeholder="password">
-                <input type="password" name="passVerify" placeholder="password verify"><br><br>
-                <input type="hidden" name="acc" value="<?php echo $_GET["acc"]; ?>">
-                <input type="submit" value="submit">
+            <form action="index.php" method="POST"> <!-- submits data to index.php using the post method  -->
+                <input type="text" name="user" placeholder="username"><!--input file for user-->
+                <input type="password" name="pass" placehoslder="password"><!--input fiele for pass-->
+                <input type="password" name="passVerify" placeholder="password verify"><br><br><!-- input files for verify the password -->
+                <input type="hidden" name="acc" value="<?php echo $_GET["acc"]; ?>"><!-- hiden input file to carry acc value from the fary sting -->
+                <input type="submit" value="submit"><!-- submit button to be able to submit the form-->
             </form>
         <?php
-        } else if ($acc == "login") {
+        } else if ($acc == "login") { /*if acc selected*/
             ?>
-            <form action="index.php" method="POST">
-                <input type="text" name="user" placeholder="username">
-                <input type="password" name="pass" placeholder="password"><br><br>
-                <input type="hidden" name="acc" value="<?php echo $_GET["acc"]; ?>">
-                <input type="submit" value="submit">
+            <form action="index.php" method="POST"> <!--submits data to index.php using post method-->
+                <input type="text" name="user" placeholder="username"><!--input file for user-->
+                <input type="password" name="pass" placeholder="password"><br><br><!--input fiele for pass-->
+                <input type="hidden" name="acc" value="<?php echo $_GET["acc"]; ?>"><!-- input files for verify the password -->
+                <input type="submit" value="submit"><!-- submit button to be able to submit the form-->
             </form>
             <?php
         }
     }
     public static function printInfo() {
-        $salt = '$6$rounds=5000$mvcproject$';
-        $query = sql("SELECT * FROM Users WHERE user = :user AND pass = :pass", [
-            ":user" => $_POST["user"],
-            ":pass" => trim(crypt($_POST["pass"], $salt), $salt)
+        $salt = '$6$rounds=5000$mvcproject$'; /*format salt*/
+        $query = sql("SELECT * FROM Users WHERE user = :user AND pass = :pass", [ /*select the data thats gonna be encypted*/
+            ":user" => $_POST["user"], /*makes ":user" = the data proccessed*/
+            ":pass" => trim(crypt($_POST["pass"], $salt), $salt) /*crypt the password using the salt format*/
         ]);
-        echo "Username: ".$_POST["user"]."<br>"; 
+        echo "Username: ".$_POST["user"]."<br>";  /*prints the username*/
         ?>
-        <a href="logout.php">Logga ut</a><br>
+        <a href="logout.php">Logga ut</a><br> <!--directs user to logout.php when pressed-->
         <?php
     }
 }
