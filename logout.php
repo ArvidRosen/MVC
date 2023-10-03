a68431c7a690c4cb61e700dc720f0ac32451255c
Kalle842 <kalle.freigie@elev.ga.ntig.se>
2023-10-03 09:22:23 +0200

    sjadföl

--- logout.php
+++ logout.php
@@ -1,13 +1,6 @@
 <?php
-<<<<<<< Updated upstream
 session_name("BAS");/* Names the session */
 session_start();/* Starts the session */
 session_destroy();/* Destroy the session */
 header("Location: index.php");/* Redirects to index.php */
-=======
-session_name("BAS"); //setting the name of the session to "BAS"
-session_start(); // start the session
-session_destroy();//destroy the session and all variables
-header("Location: index.php");// indicate the user to the index.php page
->>>>>>> Stashed changes
 ?>
